b0bb5de473465965df8f60ed875bf5bc06d47265
Rob-Sanchez-Cs <74215523+Rob-Sanchez-Cs@users.noreply.github.com>
2021-04-22 21:54:28 -0400

    Limited find by username for platform format (#13)

--- routes/platformFormat.js
+++ routes/platformFormat.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 let platformFormat = require('../models/platformFormat.model');
 
-router.route('/:username').get((req, res) => {
-    platformFormat.find({ owner: { $ne: req.params.username }, is_published: true }, 'plat_name owner is_public privacy_password cover_photo pages _id')
+router.route('/:username').post((req, res) => {
+    platformFormat.find({ owner: { $ne: req.params.username }, is_published: true }, 'plat_name owner is_public privacy_password cover_photo pages _id').skip(req.body.index*req.body.max).limit(req.body.max)
       .then(platformFormats => res.json(platformFormats))
       .catch(err => res.status(400).json('Error: ' + err));
 });

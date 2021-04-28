640718134eb0165dac5ab3558c4f3e8e0ec7ab2b
Rob-Sanchez-Cs <74215523+Rob-Sanchez-Cs@users.noreply.github.com>
2021-04-28 00:13:34 -0400

    fixed routes (#20)

--- routes/platformData.js
+++ routes/platformData.js
@@ -1,13 +1,6 @@
 const router = require('express').Router();
 let platformData = require('../models/platformData.model');
 
-//gets every platform Data
-router.route('/').get((req, res) => {
-  platformData.find()
-    .then(platformDatas => res.json(platformDatas))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
 //get platform Data's using array of Learned Platform Data
 router.route('/getLearnedPlatforms').post((req, res) => {
   platformData.find({ _id: {$in : req.body.platformDatas_id}, user_id: req.body.user_id})
@@ -60,19 +53,12 @@ router.route('/getPlatformDataCompletedPages').post((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-//gets a platform Data based on ID
-router.route('/:id').get((req, res) => {
-  platformData.findById(req.params.id)
-    .then(platformData => res.json(platformData))
-    .catch(err => res.status(400).json('Error: ' + err));
-});
-
-//deletes a platform Data based on ID
-router.route('/:id').delete((req, res) => {
-  platformData.findByIdAndDelete(req.params.id)
-      .then(() => res.json('Platform Data deleted.'))
-      .catch(err => res.status(400).json('Error: ' + err));
-});
+//deletes all of a users data schemas if we delete the user
+// router.route('/deleteSpecificPlatformData/:id').delete((req, res) => {
+//   platformData.findByIdAndDelete(req.params.id)
+//       .then(() => res.json('Platform Data deleted.'))
+//       .catch(err => res.status(400).json('Error: ' + err));
+// });
 
 //adds a platform Data entry 
 router.route('/add').post((req, res) => {
--- routes/user.js
+++ routes/user.js
@@ -7,7 +7,7 @@ router.use(cors());
 
 let user = require('../models/user.model');
 
-router.route('/').get((req, res) => {
+router.route('/getAllUsers').get((req, res) => {
     user.find()
     .then(users => res.json(users))
     .catch(err => res.status(400).json('Error: ' + err));
@@ -55,13 +55,13 @@ router.route('/getSecurityAnswer/:identifier').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/:id').get((req, res) => {
+router.route('/getSpecificUser/:id').get((req, res) => {
   user.findById(req.params.id)
     .then(user => res.json(user))
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/delete/:id').delete((req, res) => {
+router.route('/deleteSpecificUser/:id').delete((req, res) => {
   user.findByIdAndDelete(req.params.id)
       .then(() => res.json('User deleted.'))
       .catch(err => res.status(400).json('Error: ' + err));
@@ -143,36 +143,6 @@ router.post('/login', (req,res)=>{
     res.send("Error: " + err);
   })
 });
-
-
-
-
-
-
-
-router.route('/update/:id').post((req, res) => {
-  user.findById(req.params.id)
-      .then(user => {
-          user.username = req.body.username;
-          user.password = req.body.password;
-          user.email = req.body.email;
-          user.security_question = req.body.security_question;
-          user.security_answer = req.body.security_answer;
-          user.is_admin = req.body.is_admin;
-          user.created_platforms = req.body.created_platforms;
-          user.learned_platforms = req.body.learned_platforms;
-          user.profile_picture = req.body.profile_picture;
-          user.total_time_played = req.body.total_time_played;
-          user.completed_platforms = req.body.completed_platforms;
-          user.experience_points = req.body.experience_points;
-
-          user.save()
-              .then(() => res.json('User Updated!'))
-              .catch(err => res.status(400).json('Error: ' + err));
-      })
-      .catch(err => res.status(400).json('Error: ' + err));
-})
-
 
 router.post('/updatePassword/:identifier',(req, res) => {
 

4cbc519fc13d5d9e40c1da0138661f01755005d9
ChosenHero <dnnywong123@gmail.com>
2021-05-01 23:56:45 -0400

    addingRoutesCD (#29)

--- routes/categoryData.js
+++ routes/categoryData.js
@@ -23,4 +23,97 @@ router.route('/add').post((req, res) => {
     .then(() => res.json('Category Data added!'))
     .catch(err => res.status(400).json('Error: ' + err));
   });
+
+//get specific Category Data current progress pages for user id and platform id
+router.route('/getCategoryDataCurrentProgressPages').post((req, res) => {
+  categoryData.find({$and:[{user_id: req.body.id},{category_id:req.body.catid}]}, 'currentProgress_pages -_id')
+    .then(platformDatas => res.json(platformDatas[0]))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//update completed page and current progress
+router.route('/updatePageArrays/').post((req, res) => {
+  categoryData.updateOne(
+    {user_id:req.body.user_id,category_id:req.body.cat_id},
+    {$addToSet: {completed_pages:[req.body.page_id],currentProgress_pages:[req.body.page_id]}},
+
+    function(err,response)
+    {
+      if(err)
+      {
+        console.log(err)
+      }
+      else
+      {
+        res.send(response)
+      }
+    }
+  )
+})
+
+//prob wont be needing these anymore 
+//update completed Page
+router.route('/updateCompletedPage/').post((req, res) => {
+  categoryData.updateOne(
+    {user_id:req.body.user_id,category_id:req.body.cat_id},
+    {$addToSet: {completed_pages:[req.body.page_id]}},
+    function(err,response)
+    {
+      if(err)
+      {
+        console.log(err)
+      }
+      else
+      {
+        res.send(response)
+      }
+    }
+  )
+})
+
+//prob wont be needing this anymore 
+//update currentProgress
+router.route('/updateCurrentProgress/').post((req, res) => {
+  categoryData.updateOne(
+    {user_id:req.body.user_id,category_id:req.body.cat_id},
+    {$addToSet: {currentProgress_pages:[req.body.page_id]}},
+    function(err,response)
+    {
+      if(err)
+      {
+        console.log(err)
+      }
+      else
+      {
+        res.send(response)
+      }
+    }
+  )
+})
+
+//get specific category Data for user id and platform id
+router.route('/getSpecificCategoryData').post((req, res) => {
+  categoryData.find({$and:[{user_id: req.body.id},{category_id:req.body.catid}]})
+    .then(platformDatas => res.json(platformDatas))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+//set is_completed to true 
+router.route('/setCompletedTrue/').post((req, res) => {
+  categoryData.updateOne(
+    {user_id:req.body.user_id,category_id:req.body.cat_id},
+    {$set: {is_completed:true}},
+    function(err,response)
+    {
+      if(err)
+      {
+        console.log(err)
+      }
+      else
+      {
+        res.send(response)
+      }
+    }
+  )
+})
 module.exports = router;
